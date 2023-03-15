400a0ce33d390a4699fbc8b955a54958847a9f82
rahma <rahmabegag19@gmail.com>
2023-03-15 20:14:56 +0100

    registration handle errors

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,4 +2,19 @@ class RegistrationController < ApplicationController
     def new
         @account=Account.new
     end
+
+    def create
+        @account=Account.new(params_account)
+        if @account.save
+           redirect_to root_path ,notice:"Successfully created account"
+        else
+            flash[:alert]="something went wrong"
+            render :new
+        end
+    end
+
+
+    def params_account
+        params.require(:account).permit(:mail ,:passwor ,:password_confirmation)
+    end
 end
